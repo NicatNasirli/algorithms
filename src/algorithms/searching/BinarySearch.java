a97c3a08e3804f8cf2ad9e5106f4898754dc2f03
Nijat Nasirli <nicat.nasirli0511@gmail.com>
2025-01-27 20:11:47 +0400

    .

--- src/algorithms/searching/BinarySearch.java
+++ src/algorithms/searching/BinarySearch.java
@@ -3,11 +3,11 @@ package algorithms.searching;
 public class BinarySearch {
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int elementToSearch = 4;
+        int elementToSearch = 8;
         int result = binarySearch(array, elementToSearch);
 
         if (result == -1)
-            System.out.println("Element do not exist in array");
+            System.out.println("Element does not exist in the array");
         else
             System.out.println("Index of element is: " + result);
     }
@@ -17,13 +17,16 @@ public class BinarySearch {
         int right = array.length - 1;
 
         while (left <= right) {
-            int middle = (right + left) / 2;
+            int middle = left + (right - left) / 2;
 
+            if (array[middle] == element) {
+                return middle;
+            }
             if (array[middle] < element) {
                 left = middle + 1;
-            } else if (array[middle] > element) {
+            } else {
                 right = middle - 1;
-            } else return middle;
+            }
         }
 
         return -1;
