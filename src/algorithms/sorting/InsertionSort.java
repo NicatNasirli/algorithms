729644ddb2329e93473b082646422ef85b7eba52
Nijat Nasirli <nicat.nasirli0511@gmail.com>
2025-01-29 20:31:44 +0400

    Longest common prefic algorithm

--- src/algorithms/sorting/InsertionSort.java
+++ src/algorithms/sorting/InsertionSort.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class InsertionSort {
 
+    //Time complexity of O(n^2)
     public static void main(String[] args) {
         int[] array = {6, 1, 2, 7, 9, 43, 2, 10};
         System.out.println(Arrays.toString(insertionSort(array)));
